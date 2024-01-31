3f051c8b2b0df60e9555efa7d6499182da581804
Hideki <kaneki515ken@gmail.com>
2024-01-31 19:25:50 -0300

    Aula 5 - Tipo boll (boolean) - Introdução aos tipos de dados

---
+++ Curso de Python 3 do básico ao avançado - com projetos reais/aula 5.py
@@ -0,0 +1,13 @@
+# Tipo de dados boll (boolean)
+# Ao questionar algo em um programa,
+# Só existem duas respostas possíveis:
+# Sim (True) ou não (False).
+# Existem vários operadores para "questionar".
+# Dentre eles o ==, que é um operador lógico que 
+# questiona se um valor é igual a outro
+print(10 == 10) # Sim -> True (Verdadeiro)
+print(10 == 11) # Não -> False (Falso)
+print(type(True))
+print(type(False))
+print(type(10 == 10))
+print(type(10 == 11))
