a59ef1e191bf734f04c4bfe1a1cc285256f7c098
Hideki <kaneki515ken@gmail.com>
2024-01-31 17:46:15 -0300

    Aula 1 - Docustrings como comentários

---
+++ Curso de Python 3 do básico ao avançado - com projetos reais/aula 1.py
@@ -0,0 +1,21 @@
+# Permite escrever um comentário
+
+"""
+DocString
+Escrever notas, 
+pode 
+ter 
+varias 
+linhas
+"""
+
+'''
+Escrever notas 
+pode 
+ter 
+varias 
+linhas
+
+'''
+
+print('Hello World')
