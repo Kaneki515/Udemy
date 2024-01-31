6a5cc082088659e86d8404b937cdf4d685c34f31
Hideki <kaneki515ken@gmail.com>
2024-01-31 18:27:52 -0300

    Aula 3 - Tipo str (String)

---
+++ Curso de Python 3 do básico ao avançado - com projetos reais/aula 3.py
@@ -0,0 +1,24 @@
+"""
+  
+Python = Linguagem de programação
+Tipos de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+  
+"""
+
+print(1234)
+
+# Aspas simples
+print('Hideki')
+print('Shiro "Kuro"')
+
+# Aspas duplas
+print("Shiro")
+print("Shiro 'Kuro'")
+
+# Escape
+print("Shiro \"Kuro\"")
+
+# r
+print(r"Shiro \"Kuro")
