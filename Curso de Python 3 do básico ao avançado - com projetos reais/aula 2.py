ec25040253fd1c6e650219a8d1cb7380f2945458
Hideki <kaneki515ken@gmail.com>
2024-01-31 18:03:15 -0300

    aula 2 - Função String

---
+++ Curso de Python 3 do básico ao avançado - com projetos reais/aula 2.py
@@ -0,0 +1,17 @@
+#Numeros não necessitam de aspas
+print(12, 34)
+
+#String é necessario colocar aspas
+print('Teste')
+
+#sep -> serve para mudar o separador em argumentos não nomeados
+print(25, 45, sep='/')
+
+#\r\n -> CRLF
+#\n -> LF
+#São utilizados para quebra de linha
+
+#end -> final da linha ou para quebra de linha
+print(25, 45, sep='/', end=("\n"))
+
+print(25, 45, sep='/', end=("\n##"))
