9c42add0c58d157d08e8a67d5d9ba058bc896fcf
Hideki <kaneki515ken@gmail.com>
2024-02-06 21:38:46 -0300

    Aula 7 - Introdução às variáveis em Python

---
+++ Curso de Python 3 do básico ao avançado - com projetos reais/aula 7.py
@@ -0,0 +1,18 @@
+# Variáveis são usadas para salvar algo na memória do computador.
+# PEP8: inicie variáveis com letras minúsculas, pode usar
+# números e underline _.
+# O sinal é o operdor de atribuição. ELe é usado para 
+# atribuir um valor a um nome (variável).
+# Uso: nome_variavel = expressão
+
+nome_completo = 'Bruno Hideki Sato Misufara'
+print(nome_completo) # Bruno Hideki Sato Misufara
+
+soma_dois = 2 + 2
+print(soma_dois)
+
+nome = 'Hideki'
+idade = 19
+maior_idade = idade >= 18
+print('Nome:', nome, 'Idade:', idade)
+print('É maior de idade?', maior_idade)
