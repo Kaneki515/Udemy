dbeca475dacc41d8e9062745438864ef426282e8
Hideki <kaneki515ken@gmail.com>
2024-02-06 21:09:06 -0300

    Aula 6 - Coerção de tipos (convertendo um tipo para outro)

---
+++ Curso de Python 3 do básico ao avançado - com projetos reais/aula 6.py
@@ -0,0 +1,15 @@
+# Conversão de tipos, coerção
+# type convertion, typecasting, coercion
+# é o ato de converter um tipo em outro
+# tipos imutáveis e primitos:
+# str, int, float, bool
+
+print(int('1'), type(int('1')))
+
+print(int('1'), + 1)
+
+print(float('1.5') + 1) # quando inteiro + float o valor final será float
+
+print(bool('')) # boleano vazio = false
+
+print(str('11' + 'b'))
