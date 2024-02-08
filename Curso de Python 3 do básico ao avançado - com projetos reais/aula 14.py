a1904006c0f5f062c24eb7d56f309634eeda3e4b
Hideki <kaneki515ken@gmail.com>
2024-02-07 21:45:20 -0300

    Aula 14 - Solução exercício de programação - Cálculo do IMC

---
+++ Curso de Python 3 do básico ao avançado - com projetos reais/aula 14.py
@@ -0,0 +1,7 @@
+nome = 'Bruno Hideki Sato Misufara'
+altura = 1.80
+peso = 72.5
+imc = peso / (altura * altura) # IMC = peso / (altura * altura)
+
+print(nome, 'tem', altura, 'altura')
+print('pesa', peso, 'e seu IMC é', imc)
