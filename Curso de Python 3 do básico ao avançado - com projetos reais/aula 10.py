0aaf9a474579b8b80e60953288bf2dbc19f5dae6
Hideki <kaneki515ken@gmail.com>
2024-02-07 21:26:03 -0300

    Aula 10 - Introdução aos operadores aritméticos (matemática)

---
+++ Curso de Python 3 do básico ao avançado - com projetos reais/aula 10.py
@@ -0,0 +1,20 @@
+adicao = 10 + 10
+print('Adição', adicao)
+
+subtração = 10 - 5
+print('Subtração', subtração)
+
+multiplicacao = 10 * 10
+print('Multiplicação', multiplicacao)
+
+divisao = 10 / 2.2
+print('Divisão', divisao)
+
+divisao_inteira = 10 // 2
+print('Divisão inteira', divisao_inteira)
+
+exponenciacao = 2 ** 10
+print('Exponenciação', exponenciacao)
+
+modulo = 55 % 2 # resto da divisão
+print('Módulo', modulo)
