164811bd7e09e00f5e406d725e2ef23f6abd5ed5
Hideki <kaneki515ken@gmail.com>
2024-02-07 21:43:29 -0300

    Aula 13 -  Exercício de programação - Cálculo do IMC (Índice de Massa Corpórea) + Ellipsis

---
+++ Curso de Python 3 do básico ao avançado - com projetos reais/aula 13.py
@@ -0,0 +1,7 @@
+nome = 'Bruno Hideki Sato Misufara'
+altura = 1.80
+peso = 72.5
+imc = ... # IMC = peso / (altura * altura)
+
+print(nome, 'tem', altura, 'altura')
+print('pesa', peso, 'e seu IMC é', imc)
