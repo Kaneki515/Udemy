0f48a41054f14b50dcc2d99b6c8885e7bd8257db
Hideki <kaneki515ken@gmail.com>
2024-02-07 21:13:55 -0300

    Aula 9 - Solução  - exercício com variáveis e tipos de dados

---
+++ Curso de Python 3 do básico ao avançado - com projetos reais/aula 9.py
@@ -0,0 +1,13 @@
+nome = 'Bruno'
+sobrenome = 'Misufara'
+idade = 19
+ano_nascimento = 2024 - idade
+maior_idade = idade >= 18
+altura = 1.81
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_idade)
+print('Altura em metros:', altura)
