856993ee0c25a809a7e795c18a72b2241da4c4b2
Hideki <kaneki515ken@gmail.com>
2024-02-08 21:04:01 -0300

    Aula 20 - O Debugger do VS Code e o interpretador do Python lendo os códigos

---
+++ Curso de Python 3 do básico ao avançado - com projetos reais/aula 20.py
@@ -0,0 +1,21 @@
+# if / elif      /else
+# se / se não se / se não
+
+condicao1 = False
+condicao2 = True
+condicao3 = False
+condicao4 = False
+
+if condicao1:
+  print('Código para condição 1')
+elif condicao2:
+  print('Código para condição 2')
+elif condicao3:
+  print('Código para condição 3')
+elif condicao4:
+  print('Código para condição 4')
+else: 
+  print('Nenhuma das condições foi satisfeita')
+
+if 10 == 10:
+  print('Outro if')
