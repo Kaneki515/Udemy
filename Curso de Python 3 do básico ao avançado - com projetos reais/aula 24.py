4596a1468a9be4b44ad82c27d9d5d512d7382bd9
Hideki <kaneki515ken@gmail.com>
2024-02-14 18:38:39 -0300

    Aula 24 - Operador Lógico and

---
+++ Curso de Python 3 do básico ao avançado - com projetos reais/aula 24.py
@@ -0,0 +1,24 @@
+# Operadores lógicos
+# and (e) or (ou) not (não)
+# and - Todas as condições precissam ser verdadeiras
+# Se qualquer valor for considerado falso, a expressão
+# inteira será avaliada naquele valro
+# São considerados flasy (que vc já viu)
+# 0 0.0 '' False
+# Também o tipo None que é usado para representar
+# um não valor
+
+# entrada = input('[E] = Entrar || [S] = Sair: ')
+# senha = input('Senha: ')
+
+# senha_permitida = '123'
+
+# if entrada == 'E' and senha == senha_permitida:
+#   print('Entrar')
+# else:
+#   print('Sair')
+
+# Avaliação de curto circuito
+print(True and False and True)
+print(True and True)
+print(bool(''))
