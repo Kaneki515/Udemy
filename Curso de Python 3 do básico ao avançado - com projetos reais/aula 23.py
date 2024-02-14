ffd1a31413dc2e7f19c87e67def7f50d952cba49
Hideki <kaneki515ken@gmail.com>
2024-02-14 18:19:56 -0300

    Aula 23 - Solução - Exercício de programação com if e comparação

---
+++ Curso de Python 3 do básico ao avançado - com projetos reais/aula 23.py
@@ -0,0 +1,7 @@
+n1 = input('Digite um valor: ')
+n2 = input('Digite outo valor: ')
+
+if n1 >= n2:
+  print(f'primeiro valor {n1} é maior que {n2}') 
+else:
+  print(f'segundo valor {n2} é maior que {n1}') 
