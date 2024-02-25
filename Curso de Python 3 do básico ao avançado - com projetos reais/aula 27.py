5a2adcbf41b1d35e81031e2dbb25305a26deb4f4
Hideki <kaneki515ken@gmail.com>
2024-02-25 16:34:28 -0300

    Aula 27 - Interpolação de string com _ em Python

---
+++ Curso de Python 3 do básico ao avançado - com projetos reais/aula 27.py
@@ -0,0 +1,13 @@
+"""
+Interpolação básica de strings
+s - string
+d e i - int
+f - float
+e e X - Hexadeciamal
+"""
+
+nome = 'Hideki'
+preco = 1000.8520963
+variavel = '%s, o preço total foi de R$%.2f' % (nome, preco)
+print(variavel)
+print('O Hexadecimal de %d e %04X' % (1500, 1500))
